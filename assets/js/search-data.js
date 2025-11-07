4b304d9cf7d2c7701089488e78137572cdb6505d
dematerializ <46729518+dematerializ@users.noreply.github.com>
2025-11-07 11:09:08 +0000

    Deploying to gh-pages from @ dematerializ/dematerializ.github.io@0763104e8e56e5b55e82688c3a45f6f67d7012a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -434,8 +434,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-ni-fe-permalloy",
-          title: 'Ni-Fe permalloy',
+          section: "News",},{id: "projects-10-project",
+          title: '10_project',
           description: "to be updated",
           section: "Projects",handler: () => {
               window.location.href = "/projects/10_project/";
@@ -484,8 +484,8 @@ ninja.data = [{
           description: "Flickr album",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
-            },},{id: "projects-ni-based-superalloy",
-          title: 'Ni-based superalloy',
+            },},{id: "projects-9-project",
+          title: '9_project',
           description: "to be updated",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
