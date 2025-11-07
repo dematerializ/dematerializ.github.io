101ac3cdc79bfc32cf1f1246d0ca333491121ce9
dematerializ <46729518+dematerializ@users.noreply.github.com>
2025-11-07 16:17:49 +0000

    Deploying to gh-pages from @ dematerializ/dematerializ.github.io@b80bfbbb4b8feb73b1d3a10475a0b4f43dc50741 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,7 +429,12 @@ ninja.data = [{
           section: "News",},{id: "news-the-purealloys-project-is-launched-sparkles",
           title: 'The PureAlloys project is launched! :sparkles: 🎈',
           description: "",
-          section: "News",},{id: "news-talk-at-lrt-2024-conference",
+          section: "News",},{id: "news-talk-at-calphad-2024-conference",
+          title: 'Talk at CALPHAD 2024 Conference',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_7/";
+            },},{id: "news-talk-at-lrt-2024-conference",
           title: 'Talk at LRT 2024 Conference',
           description: "",
           section: "News",handler: () => {
