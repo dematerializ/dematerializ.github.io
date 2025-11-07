46cb9ccbc2a171679b3eecd724ab5b214c8c382d
dematerializ <46729518+dematerializ@users.noreply.github.com>
2025-11-07 09:16:55 +0000

    Deploying to gh-pages from @ dematerializ/dematerializ.github.io@6a43fbd36e347545a182ff589712c6fe7109f1b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,6 +461,11 @@ ninja.data = [{
           description: "Flickr album",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-relationships",
+          title: 'Relationships',
+          description: "Flickr album",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
