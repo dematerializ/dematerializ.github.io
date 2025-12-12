44e03adb6abdd31694e4dcda795c4cdc1bdeaa5f
dematerializ <46729518+dematerializ@users.noreply.github.com>
2025-12-12 15:12:26 +0000

    Deploying to gh-pages from @ dematerializ/dematerializ.github.io@aa80f5c8d426960e211abe964c2df9cfa7cf3e99 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -571,8 +571,8 @@ ninja.data = [{
           description: "Scottish Microelectronics Centre, UoEdinburgh (under construction)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-hemt-39-s-transistors-high-frequency-applications",
-          title: 'HEMT&amp;#39;s transistors (high-frequency applications)',
+            },},{id: "projects-hemt-39-s-high-frequency-transistors",
+          title: 'HEMT&amp;#39;s (high-frequency transistors)',
           description: "UoIoannina, UoGlasgow (under construction)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
