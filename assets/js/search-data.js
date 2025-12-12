7e657d132a1a480b472bc93df9a45b0e47499adc
dematerializ <46729518+dematerializ@users.noreply.github.com>
2025-12-12 10:10:48 +0000

    Deploying to gh-pages from @ dematerializ/dematerializ.github.io@67e36f45b93ba891bcef68d17563a45962be2d12 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -514,8 +514,8 @@ ninja.data = [{
           description: "funded by UKRI EU Guarantee",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-multi-scale-modelling-msm-for-battery-manufacture",
-          title: 'Multi-scale Modelling (MSM) for Battery Manufacture',
+            },},{id: "projects-multi-scale-modelling-msm-battery-manufacture",
+          title: 'Multi-scale Modelling (MSM) (Battery Manufacture)',
           description: "under construction",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
