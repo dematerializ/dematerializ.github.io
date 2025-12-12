ceb8ab896de7784cc747f88725f100f008d664df
dematerializ <46729518+dematerializ@users.noreply.github.com>
2025-12-12 10:01:56 +0000

    Deploying to gh-pages from @ dematerializ/dematerializ.github.io@87579a02f62e52fce2bb561e3ef4fd452c0d3348 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -524,8 +524,8 @@ ninja.data = [{
           description: "under construction",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-ni-fe-permalloy-as-inductors-for-mems",
-          title: 'Ni-Fe permalloy as inductors for MEMS',
+            },},{id: "projects-ni-fe-permalloy-used-as-inductors-for-mems",
+          title: 'Ni-Fe permalloy used as inductors for MEMS',
           description: "under construction",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
