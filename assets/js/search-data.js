4683ced5ba66baa2de6cef442bbeeb98e8ffeec7
dematerializ <46729518+dematerializ@users.noreply.github.com>
2025-12-12 11:34:26 +0000

    Deploying to gh-pages from @ dematerializ/dematerializ.github.io@01c69ec7c5167622019cabefdac791f1a369d9f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -531,7 +531,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-hemt-39-s-transistors-high-frequency-applications",
           title: 'HEMT&amp;#39;s transistors (high-frequency applications)',
-          description: "under construction",
+          description: "UoIoannina, UoGlasgow (under construction)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{
